de028992aecc948c038a4cb7efca4cd85866cb02
Benjamin Roziere <darkbenji58@gmail.com>
2014-07-30 02:39:54 +0200

    Add Util class Add main class Add BlockListener class

--- src/main/java/com/rdalambic/libertyprivate/LibertyPrivate.java
+++ src/main/java/com/rdalambic/libertyprivate/LibertyPrivate.java
@@ -3,5 +3,27 @@ package com.rdalambic.libertyprivate;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LibertyPrivate extends JavaPlugin {
-
+	
+	//private static boolean enabled = false;
+	
+	@Override
+	public void onEnable()
+	{
+		if(isEnabled()) return;
+		
+		super.onEnable();
+		
+		setEnabled(true);
+	}
+	
+	public void onDisable()
+	{
+		if(!isEnabled()) return;
+		
+		super.onDisable();
+		
+		getLogger().info(" is being disabled.");
+		
+		setEnabled(false);
+	}
 }
